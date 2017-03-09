1181824bce3154b8af22a31579972d243b3adaa6
Vincent Rasquier <vrasquier@keley-live.com>
2017-03-09 15:15:49 +0100

    Little update on CAS [Not tested - not working]

--- src/services/api/cas-auth.js
+++ src/services/api/cas-auth.js
@@ -1,14 +1,11 @@
 import fetchJsonp from 'fetch-jsonp'
 import { Observable } from 'rxjs'
 
-const API_URL = "https://cas-auth-provider.herokuapp.com/"
-const CAS_URL = "https://perso-etudiant.u-pem.fr/~vrasquie/cas/"
+const SERVICE_UID = "{uid}"//"https://cas-auth-provider.herokuapp.com/"
+const CAS_URL = "https://perso-etudiant.u-pem.fr/~vrasquie/cas/api/service/call"
 
-export const CAS_URL_FORCE = CAS_URL + "?force=true"
-export const CAS_URL_ALLOW = CAS_URL + "allow.php?service=" + API_URL
-
-export const checkAuth = (ticket) => {
-    var url = CAS_URL + "?service=" + API_URL;
+export const checkAuth = () => {
+    var url = CAS_URL + "/" + API_URL;
 
     return Observable.from(fetchJsonp(url))
     .flatMap(response => response.json())
