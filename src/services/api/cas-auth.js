eb1de7dc360e16124ff6051da2a2369d43f7289c
Vincent Rasquier <vrasquier@keley-live.com>
2017-03-10 16:25:36 +0100

    Try to rethink the API of CAS auth

--- src/services/api/cas-auth.js
+++ src/services/api/cas-auth.js
@@ -1,13 +1,15 @@
 import fetchJsonp from 'fetch-jsonp'
 import { Observable } from 'rxjs'
 
-const SERVICE_UID = "{uid}"//"https://cas-auth-provider.herokuapp.com/"
+const SERVICE_UID = "acf2feb0-05a4-11e7-93ae-92361f002671"
 const CAS_URL = "https://perso-etudiant.u-pem.fr/~vrasquie/cas/api/service/call"
 
 export const checkAuth = () => {
-    var url = CAS_URL + "/" + API_URL;
+    var url = CAS_URL + "/" + SERVICE_UID;
 
     return Observable.from(fetchJsonp(url))
-    .flatMap(response => response.json())
+    .flatMap(response => {
+        console.log(response)
+        return response.json()})
     .map(response => JSON.parse(response))
 }
